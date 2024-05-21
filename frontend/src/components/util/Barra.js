2c40ba40ca4c6ec7d14065a29d77f0c8515e9197
corrumptus <lazarinilucas42@gmail.com>
2024-05-21 15:08:12 -0300

    feat: creating the action bar component

---
+++ frontend/src/components/util/Barra.js
@@ -0,0 +1,54 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+
+const Barra = () => {
+  const [ submenuAberto, setSubmenuAberto ] = useState(null);
+
+  function toggleSubmenu(grupo) {
+    setSubmenuAberto((prevState) => (prevState === grupo ? null : grupo));
+  };
+
+  function handleClick(grupo) {
+    return (e) => {
+      e.preventDefault();
+      toggleSubmenu(grupo);
+    }
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="navbar-nav">
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="/"
+              id="sorvetesDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded={submenuAberto === 'sorvetes'}
+              onClick={handleClick('sorvetes')}
+            >
+              Sorvetes
+            </a>
+            <div
+              className={`dropdown-menu ${submenuAberto === 'sorvetes' ? 'show' : ''}`}
+              aria-labelledby="sorvetesDropdown"
+            >
+              <Link className="dropdown-item" to="/sorvetes">
+                Cadastrar Sorvetes
+              </Link>
+              <Link className="dropdown-item" to="/sorvetes/listar">
+                Listar Sorvetes
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Barra;
