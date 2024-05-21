07c524ef794ccf6d33443d3103eb00c75dc6432b
corrumptus <lazarinilucas42@gmail.com>
2024-05-21 15:56:53 -0300

    feat: adding new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import { Sorvetes } from './components/Sorvete/CadastrarSorvete';
+import Listar from "./components/Sorvete/ListarSorvete";
+import EditarSorvete from './components/Sorvete/EditarSorvetes';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/sorvetes" element={<Sorvetes />} />
+        <Route path="/" element={<Listar />} />
+        <Route path="/sorvete" element={<Sorvetes />} />
+        <Route path="/sorvete/:id" element={<EditarSorvete />} />
       </Routes>
     </Router>
   );
