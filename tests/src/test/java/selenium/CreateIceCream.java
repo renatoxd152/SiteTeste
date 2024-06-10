58eaa334a8b8ecb6a3a381089ab3d30dd63e28cc
Brun0HC <bruno.cobuci@biotronica.tech>
2024-06-09 21:29:33 -0300

    test: initialize setup for tests

--- tests/src/test/java/selenium/CreateIceCream.java
+++ tests/src/test/java/selenium/CreateIceCream.java
@@ -1,2 +1,21 @@
-package selenium;public class CreateIceCream {
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class CreateIceCream {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        WebDriverManager.edgedriver().setup();
+        driver= new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("site-teste-renatoxd152s-projects.vercel.app");
+    }
+
 }
