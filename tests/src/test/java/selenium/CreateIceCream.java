2cb8bb9199ffe9a6cd8e3b42616ee8e2c982c564
Brun0HC <bruno.cobuci@biotronica.tech>
2024-06-10 12:37:12 -0300

    test: complete create ice cream test

--- tests/src/test/java/selenium/CreateIceCream.java
+++ tests/src/test/java/selenium/CreateIceCream.java
@@ -11,12 +11,31 @@ public class CreateIceCream extends BaseTest{
     //Opções de By
     By sorvetesMenuDropdown = By.xpath("//a[@id='sorvetesDropdown']");
     By cadatrarSorvete = By.xpath("//div[@id='root']//a[text()='Cadastrar Sorvetes']");
+    By nomeField = By.xpath("//input[@class='form-control' and @type='text']");
+    By quantidadeField = By.xpath("//input[@class='form-control' and @type='number'][1]");
+    By precoField = By.xpath("//input[@class='form-control' and @type='number'][2]");
+    By cadastrarButton = By.xpath("//div[@id='root']//button[text()='Cadastrar Sorvete']");
     @Test
     @DisplayName("Clica em criar novo sorvete")
     public void ClicarEmCriarNovoSorvete() throws InterruptedException {
         click(sorvetesMenuDropdown);
         wait.until(ExpectedConditions.visibilityOfElementLocated(cadatrarSorvete));
         click(cadatrarSorvete);
+
+        String nomeSorvete = faker.food().ingredient();
+        int quantidade = faker.number().numberBetween(1, 100);
+        double preco = faker.number().randomDouble(2, 1, 50);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(nomeField));
+        find(nomeField).sendKeys(nomeSorvete);
+
+        find(quantidadeField).clear();
+        find(quantidadeField).sendKeys(String.valueOf(quantidade));
+
+        find(precoField).clear();
+        find(precoField).sendKeys(String.valueOf(preco));
+        //Thread.sleep(4000);
+        click(cadastrarButton);
     }
 
     private WebElement find(By locator) {
