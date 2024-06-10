32c45840a014600db186c818a9dded2f132a0fb8
Brun0HC <bruno.cobuci@biotronica.tech>
2024-06-10 11:39:08 -0300

    test: update create ice cream test

--- tests/src/test/java/selenium/CreateIceCream.java
+++ tests/src/test/java/selenium/CreateIceCream.java
@@ -1,42 +1,30 @@
 package selenium;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class CreateIceCream {
+public class CreateIceCream extends BaseTest{
 
-    protected static WebDriver driver;
-
-    @BeforeEach
-    public void setUp(){
-        WebDriverManager.edgedriver().setup();
-        EdgeOptions options = new EdgeOptions();
-        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-web-security");
-        driver = new EdgeDriver(options);
-        driver.manage().window().maximize();
-        driver.get("https://site-teste-renatoxd152s-projects.vercel.app");
-    }
-
+    //Opções de By
+    By sorvetesMenuDropdown = By.xpath("//a[@id='sorvetesDropdown']");
+    By cadatrarSorvete = By.xpath("//div[@id='root']//a[text()='Cadastrar Sorvetes']");
     @Test
     @DisplayName("Clica em criar novo sorvete")
     public void ClicarEmCriarNovoSorvete() throws InterruptedException {
-        driver.findElement(By.xpath("//a[@id='sorvetesDropdown']")).click();
-        //Thread.sleep(5000);
+        click(sorvetesMenuDropdown);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(cadatrarSorvete));
+        click(cadatrarSorvete);
     }
 
-
-    @AfterAll
-    public static void tearDown(){
-        if (driver != null)
-            driver.quit();
+    private WebElement find(By locator) {
+        return driver.findElement(locator);
     }
+
+    private void click(By locator) {
+        find(locator).click();
+    }
+
 }
