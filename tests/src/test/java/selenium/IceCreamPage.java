3c4b11aebc653d704dd1045046833d6d9762caac
Brun0HC <bruno.cobuci@biotronica.tech>
2024-06-10 22:04:17 -0300

    feat: create ice cream page object

--- tests/src/test/java/selenium/IceCreamPage.java
+++ tests/src/test/java/selenium/IceCreamPage.java
@@ -1,18 +1,62 @@
 package selenium;
 
-import com.github.javafaker.Faker;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.time.Duration;
+public class IceCreamPage extends BasePage {
 
-public class BasePage {
+    public IceCreamPage(WebDriver driver) {
+        super(driver);
+    }
 
+    String nomeSorvete = faker.food().ingredient();
+    int quantidade = faker.number().numberBetween(1, 100);
+    double preco = faker.number().randomDouble(2, 1, 50);
 
+    By sorvetesMenuDropdown = By.xpath("//a[@id='sorvetesDropdown']");
+    By cadatrarSorvete = By.xpath("//div[@id='root']//a[text()='Cadastrar Sorvetes']");
+    By nomeField = By.xpath("//input[@class='form-control' and @type='text']");
+    By quantidadeField = By.xpath("//input[@class='form-control' and @type='number'][1]");
+    By precoField = By.xpath("//input[@class='form-control' and @type='number'][2]");
+    By cadastrarButton = By.xpath("//div[@id='root']//button[text()='Cadastrar Sorvete']");
 
+    private WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public void menu() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(sorvetesMenuDropdown));
+        find(sorvetesMenuDropdown).click();
+    }
+
+    public void cadastraMenu() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(cadatrarSorvete));
+        find(cadatrarSorvete).click();
+    }
+
+    public void addNome() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(nomeField));
+        find(nomeField).sendKeys(nomeSorvete);
+    }
+
+    public void addQtd() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(quantidadeField));
+        find(quantidadeField).clear();
+        find(quantidadeField).sendKeys(String.valueOf(quantidade));
+        wait.until(ExpectedConditions.textToBePresentInElementValue(quantidadeField, String.valueOf(quantidade)));
+    }
+
+    public void addPreco() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(precoField));
+        find(precoField).clear();
+        find(precoField).sendKeys(String.valueOf(preco));
+        wait.until(ExpectedConditions.textToBePresentInElementValue(precoField, String.valueOf(preco)));
+    }
+
+    public void cadastra() {
+        wait.until(ExpectedConditions.elementToBeClickable(cadastrarButton));
+        find(cadastrarButton).click();
+    }
 }
