30d2de5fc7dc446a515f570b27c9e52aefd272dd
Brun0HC <bruno.cobuci@biotronica.tech>
2024-06-10 22:04:54 -0300

    test: refactor create ice cream test

--- tests/src/test/java/selenium/IceCreamTest.java
+++ tests/src/test/java/selenium/IceCreamTest.java
@@ -1,29 +1,20 @@
 package selenium;
 
-import com.github.javafaker.Faker;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-import java.time.Duration;
-
-public class BasePageTest {
-
-    protected WebDriver driver;
-    protected WebDriverWait wait;
-    protected Faker faker;
+public class IceCreamTest {
+    private WebDriver driver;
+    private IceCreamPage iceCreamPage;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         WebDriverManager.edgedriver().setup();
         EdgeOptions options = new EdgeOptions();
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
@@ -32,58 +23,24 @@ public class BasePageTest {
         driver = new EdgeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://site-teste-renatoxd152s-projects.vercel.app");
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-
-        faker = new Faker();
+        iceCreamPage = new IceCreamPage(driver);
     }
 
     @AfterEach
-    public void tearDown(){
-        if (driver != null)
+    public void tearDown() {
+        if (driver != null) {
             driver.quit();
+        }
     }
 
-    //Opções de By
-    By sorvetesMenuDropdown = By.xpath("//a[@id='sorvetesDropdown']");
-    By cadatrarSorvete = By.xpath("//div[@id='root']//a[text()='Cadastrar Sorvetes']");
-    By nomeField = By.xpath("//input[@class='form-control' and @type='text']");
-    By quantidadeField = By.xpath("//input[@class='form-control' and @type='number'][1]");
-    By precoField = By.xpath("//input[@class='form-control' and @type='number'][2]");
-    By cadastrarButton = By.xpath("//div[@id='root']//button[text()='Cadastrar Sorvete']");
     @Test
-    @DisplayName("Clica em criar novo sorvete e insere um sorvete valido")
-    public void ClicarEmCriarNovoSorvete() throws InterruptedException {
-        click(sorvetesMenuDropdown);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(cadatrarSorvete));
-        click(cadatrarSorvete);
-
-        String nomeSorvete = faker.food().ingredient();
-        int quantidade = faker.number().numberBetween(1, 100);
-        double preco = faker.number().randomDouble(2, 1, 50);
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(nomeField));
-        find(nomeField).sendKeys(nomeSorvete);
-
-        find(quantidadeField).clear();
-        find(quantidadeField).sendKeys(String.valueOf(quantidade));
-        wait.until(ExpectedConditions.textToBePresentInElementValue(quantidadeField, String.valueOf(quantidade)));
-
-        find(precoField).clear();
-        find(precoField).sendKeys(String.valueOf(preco));
-        wait.until(ExpectedConditions.textToBePresentInElementValue(precoField, String.valueOf(preco)));
-
-        click(cadastrarButton);
+    @DisplayName("Clica em criar novo sorvete e insere um sorvete válido")
+    public void ClicarEmCriarNovoSorvete() {
+        iceCreamPage.menu();
+        iceCreamPage.cadastraMenu();
+        iceCreamPage.addNome();
+        iceCreamPage.addQtd();
+        iceCreamPage.addPreco();
+        iceCreamPage.cadastra();
     }
-
-
-
-    private WebElement find(By locator) {
-        return driver.findElement(locator);
-    }
-
-    private void click(By locator) {
-        find(locator).click();
-    }
-
 }
